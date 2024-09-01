a089b02f0e942687522ecbdce972e2a7f555bdc0
muhamad farhan <farhan.kzsr23@gmail.com>
2024-09-01 19:31:47 +0700

    sql user dump

---
+++ dump[user].sql
@@ -0,0 +1,3 @@
+INSERT INTO csie.dbo.[user] (username,password,[role],district_id,created_at,updated_at,deleted_at,nama) VALUES
+	 (N'0000000001',N'$2b$10$Fp6GomIiycm91J1JT0mI5e26WeCykmOpFAMD9ZxNKmXFFjwaozxka',1,NULL,'2024-08-31 14:30:09.0000000','2024-08-31 14:30:09.0000000',NULL,N'Ujang'),
+	 (N'0000000002',N'$2b$10$Fp6GomIiycm91J1JT0mI5e26WeCykmOpFAMD9ZxNKmXFFjwaozxka',2,2,'2024-09-01 04:31:13.6100000','2024-09-01 10:38:53.3700000',NULL,N'Suep');
